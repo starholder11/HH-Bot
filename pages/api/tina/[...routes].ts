bef46e45d9697a03a08582511bb19c41ce521e68
CFX <c_fur1@Chriss-MacBook-Pro.local>
2025-07-03 20:25:33 -0400

    Switch to TinaCMS built-in credentials authentication

--- pages/api/tina/[...routes].ts
+++ pages/api/tina/[...routes].ts
@@ -1,18 +1,6 @@
 import { TinaNodeBackend } from '@tinacms/datalayer'
-import { AuthJsBackendAuthProvider, TinaAuthJSOptions } from 'tinacms-authjs'
 import databaseClient from '../../../tina/__generated__/databaseClient'
 
-// Validate required environment variables
-if (!process.env.NEXTAUTH_SECRET) {
-  throw new Error('NEXTAUTH_SECRET environment variable is required')
-}
-
 export default TinaNodeBackend({
-  authProvider: AuthJsBackendAuthProvider({
-    authOptions: TinaAuthJSOptions({
-      databaseClient,
-      secret: process.env.NEXTAUTH_SECRET,
-    }),
-  }),
   databaseClient,
 }) 
