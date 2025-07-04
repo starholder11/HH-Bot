32019453c39355857d354eb65a7026a4fd213a1a
CFX <c_fur1@Chriss-MacBook-Pro.local>
2025-07-03 21:22:13 -0400

    Add required authProvider for TinaCMS Cloud configuration

--- pages/api/tina/[...routes].ts
+++ pages/api/tina/[...routes].ts
@@ -1,6 +1,13 @@
 import { TinaNodeBackend } from '@tinacms/datalayer'
+import { AuthJsBackendAuthProvider, TinaAuthJSOptions } from 'tinacms-authjs'
 import databaseClient from '../../../tina/__generated__/databaseClient'
 
 export default TinaNodeBackend({
+  authProvider: AuthJsBackendAuthProvider({
+    authOptions: TinaAuthJSOptions({
+      databaseClient,
+      secret: process.env.NEXTAUTH_SECRET || 'fallback-secret',
+    }),
+  }),
   databaseClient,
 }) 
