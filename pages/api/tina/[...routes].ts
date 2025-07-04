87dda483d236570e2116d0cbfb5a5400bf8baa88
CFX <c_fur1@Chriss-MacBook-Pro.local>
2025-07-03 21:12:03 -0400

    Switch to TinaCMS Cloud configuration with proper authentication

--- pages/api/tina/[...routes].ts
+++ pages/api/tina/[...routes].ts
@@ -1,7 +1,6 @@
-import { TinaNodeBackend, LocalBackendAuthProvider } from '@tinacms/datalayer'
+import { TinaNodeBackend } from '@tinacms/datalayer'
 import databaseClient from '../../../tina/__generated__/databaseClient'
 
 export default TinaNodeBackend({
-  authProvider: LocalBackendAuthProvider(), // Built-in, no auth needed for testing
   databaseClient,
 }) 
