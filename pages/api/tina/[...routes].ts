0c308de0d86f1ed23ff5254b8b65808b945b5564
CFX <c_fur1@Chriss-MacBook-Pro.local>
2025-07-03 20:27:22 -0400

    Add required authProvider to TinaNodeBackend configuration

--- pages/api/tina/[...routes].ts
+++ pages/api/tina/[...routes].ts
@@ -2,5 +2,15 @@ import { TinaNodeBackend } from '@tinacms/datalayer'
 import databaseClient from '../../../tina/__generated__/databaseClient'
 
 export default TinaNodeBackend({
+  authProvider: {
+    type: 'credentials',
+    users: [
+      {
+        name: 'spaceman',
+        email: 'cfurlong@gmail.com',
+        password: 'admin123',
+      },
+    ],
+  },
   databaseClient,
 }) 
