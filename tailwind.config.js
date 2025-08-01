64b8289561df2d13e9de72bae534eda909a8b530
CFX <c_fur1@Chriss-MacBook-Pro.local>
2025-07-31 22:58:30 -0400

    Fix Tailwind config to CommonJS; install missing UI deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+const config = {
     darkMode: ['class'],
     content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -149,4 +148,4 @@ const config: Config = {
 ],
 }
 
-export default config 
+module.exports = config 
