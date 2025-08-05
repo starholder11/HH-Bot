53d73d3585cc59c4f3107f47f648022d0c6e57e9
CFX <c_fur1@Chriss-MacBook-Pro.local>
2025-08-05 10:57:10 -0400

    fix: add nodejs runtime to Keystatic route for Vercel compatibility

--- app/api/keystatic/[...params]/route.ts
+++ app/api/keystatic/[...params]/route.ts
@@ -1,6 +1,9 @@
 import { makeRouteHandler } from '@keystatic/next/route-handler';
 import config from '../../../../keystatic.config';
 
+export const runtime = 'nodejs';
+export const dynamic = 'force-dynamic';
+
 export const { POST, GET } = makeRouteHandler({
   config,
 }); 
