f3f783bfbabc8afa9ab06730806961f58e556259
CFX <c_fur1@Chriss-MacBook-Pro.local>
2025-08-05 13:44:23 -0400

    fix: actually add slug field to timeline schema

--- keystatic.config.tsx
+++ keystatic.config.tsx
@@ -29,6 +29,7 @@ export default config({
       path: 'content/timeline/*/',
       slugField: 'slug',
       schema: {
+        slug: fields.slug({ name: { label: 'Slug' } }),
         title: fields.text({ label: 'Title' }),
         date: fields.date({ label: 'Date' }),
         content: fields.mdx({ label: 'Content' }),
