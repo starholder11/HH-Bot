64c32cc6babb30f151134982e1202eff1febd044
CFX <c_fur1@Chriss-MBP.lan>
2025-08-20 22:33:22 -0400

    Fix R3F test component: remove dynamic imports, add proper useFrame animation and OrbitControls

--- components/spatial/test-r3f.tsx
+++ components/spatial/test-r3f.tsx
@@ -1,18 +1,33 @@
 "use client";
-import dynamic from 'next/dynamic';
+import { useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
-// Dynamic import to avoid SSR issues with Three.js
-const Canvas = dynamic(() => import('@react-three/fiber').then(mod => ({ default: mod.Canvas })), { ssr: false });
-const Box = dynamic(() => import('@react-three/drei').then(mod => ({ default: mod.Box })), { ssr: false });
+function RotatingBox() {
+  const meshRef = useRef<any>();
+  
+  useFrame((state, delta) => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += delta;
+      meshRef.current.rotation.y += delta * 0.5;
+    }
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  );
+}
 
 function TestScene() {
   return (
-    <Canvas style={{ height: '400px', width: '100%' }}>
+    <Canvas style={{ height: '400px', width: '100%' }} camera={{ position: [3, 3, 3] }}>
       <ambientLight intensity={0.5} />
       <pointLight position={[10, 10, 10]} />
-      <Box args={[1, 1, 1]} position={[0, 0, 0]}>
-        <meshStandardMaterial color="orange" />
-      </Box>
+      <RotatingBox />
+      <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
     </Canvas>
   );
 }
